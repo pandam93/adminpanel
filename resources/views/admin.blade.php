872a13d8fa48bfe6727cda16b6e98bb1b3262c47
Carlos Millan <carlos@dinamiq.com>
2022-05-11 13:35:03 +0200

    la forma buena

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -29,28 +29,52 @@ const data = {
   labels: labels,
   datasets: [{
     label: 'My First Dataset',
-    data: {!! json_encode($months) !!},
+    data: {!! $months !!},
     backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)',
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 99, 132)',
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
     ],
-    hoverOffset: 4
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(75, 192, 192)',
+    ],
+    borderWidth: 1
   }]
 };
 
 const config = {
-  type: 'pie',
+  type: 'bar',
   data: data,
+  options: {
+    scales: {
+      yAxes: [{
+          display: true,
+          ticks: {
+              beginAtZero: true
+          }
+      }]
+    }
+  },
 };
 
   const myChart = new Chart(
--- routes/web.php
+++ routes/web.php
@@ -18,32 +18,21 @@ Route::get('/', function () {
 });
 
 Route::get('/admin', function() {
-    $users = \App\Models\User::get(['created_at']);
-
-    //$users = \App\Models\User::whereYear('created_at', '=', Carbon\Carbon::now()->year)->get(['created_at']);
-    $months = $users->map(function ($item, $key) {
-        return $item->created_at->month;
+    $users = \App\Models\User::whereYear('created_at', '2021')->get();
+    
+    $dates = $users->pluck('created_at');
+    
+    $datesFormatted = $dates->map(function($date){
+        return $date->month -1;
     });
 
-    $monthGraph = [ 0 => 0,
-                    1 => 0,
-                    2 => 0,
-                    3 => 0,
-                    4 => 0,
-                    5 => 0,
-                    6 => 0,
-                    7 => 0,
-                    8 => 0,
-                    9 => 0,
-                    10 => 0,
-                    11 => 0 ];
-    
-    foreach($months->sort()->all() as $value) {
-        
-        $monthGraph[$value -1] += 1;
+    $months = $datesFormatted->countBy()->sortKeys();
+
+    if($months->first() != 0) {
+        $data = $months->pad(-12, 0);
     }
 
-    //dd($monthGraph, $months->sort()->all());
-    
-    return view('admin')->with('months', $monthGraph);
+    // dd($data->toJson());
+
+    return view('admin')->with('months', $data->toJson());
 });
