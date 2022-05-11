da4580ced55b19478fd9e6d49e91f3b224e3e533
Carlos Millan <carlos@dinamiq.com>
2022-05-11 19:09:55 +0200

    testing layouts y como mostrar las graficas

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -9,12 +9,89 @@
 @stop
 
 @section('content')
+<div class="alert alert-success" role="alert">
+  This is a success alert—check it out!
+</div>
     <p>Welcome to this beautiful admin panel.</p>
 
-    <div>
-        <canvas id="myChart"></canvas>
+    <h2>Sin cointainer</h2>
+    <div class="mt-2 row">
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col-md-4" style="height: 50px; border: 1px green solid"></div>
+          <div class="col-md-4" style="height: 50px; border: 1px red solid"></div>
+          <div class="col-md-4" style="height: 50px; border: 1px blue solid"></div>
+        </div>
+      </div>
     </div>
-      
+    <hr>
+    
+    <h2>Container fluid</h2>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4" style="height: 50px; border: 1px green solid"></div>
+        <div class="col-md-4 h-auto" style="height: 50px; border: 1px red solid">
+          <canvas id="myChart"></canvas>
+          <div class="ribbon-wrapper ribbon-lg">
+            <div class="ribbon bg-success text-lg">
+              Ribbon
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4" style="height: 50px; border: 1px blue solid"></div>
+      </div>
+    </div>
+    <hr>
+
+    <h2>Container</h2>
+    <div class="mt-2 container">
+      <div class="row">
+        <div class="col-md-4" style="height: 50px; border: 1px green solid"></div>
+        <div class="col-md-4" style="height: 50px; border: 1px red solid"></div>
+        <div class="col-md-4" style="height: 50px; border: 1px blue solid"></div>
+      </div>
+    </div>
+    <hr>
+
+    <h2>Cards</h2>
+    <div class="mt-2 container-fluid">
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">Default Card Example</h3>
+              <div class="card-tools">
+                <!-- Buttons, labels, and many other things can be placed here! -->
+                <!-- Here is a label for example -->
+                <span class="badge badge-primary">Label</span>
+              </div>
+              <!-- /.card-tools -->
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+              The body of the card
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">
+              The footer of the card
+            </div>
+            <!-- /.card-footer -->
+          </div>
+          <!-- /.card -->
+          
+        </div>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
 @stop
 
 @section('css')
@@ -28,7 +105,7 @@ const labels = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto
 const data = {
   labels: labels,
   datasets: [{
-    label: 'My First Dataset',
+    label: 'Usuarios creados:',
     data: {!! $months !!},
     backgroundColor: [
       'rgba(255, 99, 132, 0.2)',
